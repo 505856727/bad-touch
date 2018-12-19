6d61189b536fdead43c4bb4a538ff08ff7fbcd35
505856727 <30371192+505856727@users.noreply.github.com>
2018-12-19 21:30:04 +0800

    搬迁帧动画资源及一些小测试

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public GameManager gameManager;
+    [SerializeField]
+    private float movespeed;
+    private void FixedUpdate()
+    {
+        CharacterMove();
+    }
+    void CharacterMove()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, movespeed, 0);
+        }
+        else
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public enum Platform { windows, ios, andriod };
+    private Platform platform;
+    private void Awake()
+    {
+#if UNITY_ANDROID
+        platform = Platform.andriod;
+#endif
+
+#if UNITY_IPHONE
+        platform = Platform.ios;
+#endif
+
+#if UNITY_STANDALONE_WIN
+        platform = Platform.windows;
+#endif
+    }
+
+    public Platform GetPlatform()
+    {
+        return platform;
+    }
+}
